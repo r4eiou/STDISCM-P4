d148f0a185abdb1c93d0113132c90424eee9edf7
Althea <garciareinaalthea@gmail.com>
2025-11-30 11:33:14 +0800

    remove unused file

--- login/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'; 
-import dotenv from 'dotenv'; dotenv.config(); 
-
-// Use service_role key from Supabase settings 
-export const supabase = createClient( 
-    process.env.SUPABASE_URL, 
-    process.env.SUPABASE_SERVICE_KEY 
-);
