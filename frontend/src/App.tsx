fd6ab86f543741f807cc5783c87d4f79422664ee
clarissaalbarr <cmalbarracin15@gmail.com>
2025-12-03 10:58:28 +0800

    Fixed mode switching (persists page refreshes)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import AccountForm from "./components/AccountForm";
-import { ThemeProvider } from "./components/ThemeProvider";
 import Dashboard from "./components/Dashboard";
 
 import { useAccount } from "@/AccountContext";
@@ -10,25 +9,23 @@ function App() {
     useAccount();
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <section className=" w-full flex flex-row items-center justify-center h-full">
-        {!accountType && (
-          <>
-            <section className="relative flex flex-col items-center justify-center w-3/5  h-full">
-              <div className="absolute inset-0 bg-[url(/images/pexels-polina-zimmerman-3747505.jpg)] bg-cover bg-center brightness-60 -z-10" />
+    <section className=" w-full flex flex-row items-center justify-center h-full">
+      {!accountType && (
+        <>
+          <section className="relative flex flex-col items-center justify-center w-3/5  h-full">
+            <div className="absolute inset-0 bg-[url(/images/pexels-polina-zimmerman-3747505.jpg)] bg-cover bg-center brightness-60 -z-10" />
 
-              <span className="font-bold text-5xl text-white drop-shadow-lg">
-                Welcome to Shiz University!
-              </span>
-            </section>
-            <section className="flex flex-col items-center justify-center w-2/5 bg-primary h-full">
-              <AccountForm />
-            </section>
-          </>
-        )}
-        {accountType && <Dashboard />}
-      </section>
-    </ThemeProvider>
+            <span className="font-bold text-5xl text-white drop-shadow-lg">
+              Welcome to Shiz University!
+            </span>
+          </section>
+          <section className="flex flex-col items-center justify-center w-2/5 bg-primary h-full">
+            <AccountForm />
+          </section>
+        </>
+      )}
+      {accountType && <Dashboard />}
+    </section>
   );
 }
 
